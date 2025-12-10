170012b6335f46d9111403fea0beb87fbf564af1
abdullahsheikh6835-beep <abdullahsheikh6835@gmail.com>
2025-12-11 04:56:51 +0500

    'api store'

--- lib/repo/todos_repo.dart
+++ lib/repo/todos_repo.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:developer';
 
 import 'package:flutter_application_1/utils/app_urls.dart';
 import 'package:http/http.dart' as abdullah;
@@ -8,22 +7,45 @@ class TodosRepo {
   Future<List<Map<String, dynamic>>?> getTodoNotes() async {
     try {
       List<Map<String, dynamic>>? result;
-
       var response = await abdullah.get(Uri.parse(AppUrls.getTodos));
-
       if (response.statusCode == 200 || response.statusCode == 201) {
         final List<dynamic> decodedList = jsonDecode(response.body);
         result = decodedList.cast<Map<String, dynamic>>();
-        log(result.toString());
         return result;
-      } else {
-        log("error");
       }
-      return result;
     } catch (e) {
-      log(e.toString());
-
       rethrow;
     }
   }
 }
+
+// import 'dart:convert';
+// // import 'dart:developer';
+
+// import 'package:flutter_application_1/utils/app_urls.dart';
+// import 'package:http/http.dart' as abdullah;
+
+// class TodosRepo {
+//   Future<List<Map<String, dynamic>>?> getTodoNotes() async {
+//     try {
+//       List<Map<String, dynamic>>? result;
+
+//       var response = await abdullah.get(Uri.parse(AppUrls.getTodos));
+
+//       if (response.statusCode == 200 || response.statusCode == 201) {
+//         final List<dynamic> decodedList = jsonDecode(response.body);
+//         result = decodedList.cast<Map<String, dynamic>>();
+//         // log(result.toString());
+//         return result;
+//       }
+//       //  else {
+//       //   log("error");
+//       // }
+//       // return result;
+//     } catch (e) {
+//       // log(e.toString());
+
+//       rethrow;
+//     }
+//   }
+// }
--- lib/view/todos_view.dart
+++ lib/view/todos_view.dart
@@ -9,40 +9,35 @@ class TodosView extends StatefulWidget {
 }
 
 class _TodosViewState extends State<TodosView> {
-  List<Map<String, dynamic>>? response;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("OUr todos result")),
+      appBar: AppBar(title: Text('todos view')),
       body: Column(
         children: [
           FutureBuilder(
             future: TodosRepo().getTodoNotes(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
+            builder: (context, Snapshot) {
+              if(Snapshot.connectionState == ConnectionState.waiting ){
                 return CircularProgressIndicator();
               }
-              if (!snapshot.hasData) {
-                return Text("no data found");
+              if (!Snapshot.hasData) {
+                return Text(' no data found');
               }
-              if (snapshot.hasError) {
-                return Text("get error");
+              if (Snapshot.hasData) {
+                return Text(' data found');
               }
-
-              final data = snapshot.data;
-              return Expanded(
-                child: ListView.builder(
-                  itemCount: data?.length,
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
+              final data = Snapshot.data;
+              return ListView.builder( 
+                itemCount: data?.length ,
+                itemBuilder: (context , index){
                     final result = data?[index];
-                    return ListTile(
-                      title: Text(result?["userId"].toString() ?? ''),
+                return ListTile(
+                      title: Text(result ?["userId"].toString() ??'') ,
                       subtitle: Text(result?["title"].toString() ?? ''),
-                    );
-                  },
-                ),
-              );
+
+                );
+              });
             },
           ),
         ],
@@ -50,3 +45,58 @@ class _TodosViewState extends State<TodosView> {
     );
   }
 }
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_application_1/repo/todos_repo.dart';
+
+// class TodosView extends StatefulWidget {
+//   const TodosView({super.key});
+
+//   @override
+//   State<TodosView> createState() => _TodosViewState();
+// }
+
+// class _TodosViewState extends State<TodosView> {
+//   List<Map<String, dynamic>>? response;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text("OUr todos result")),
+//       body: Column(
+//         children: [
+//           FutureBuilder(
+//             future: TodosRepo().getTodoNotes(),
+//             builder: (context, snapshot) {
+//               if (snapshot.connectionState == ConnectionState.waiting) {
+//                 return CircularProgressIndicator();
+//               }
+//               if (!snapshot.hasData) {
+//                 return Text("no data found");
+//               }
+//               if (snapshot.hasError) {
+//                 return Text("get error");
+//               }
+
+//               final data = snapshot.data;
+//               return Expanded(
+//                 child: ListView.builder(
+//                   itemCount: data?.length,
+//                   shrinkWrap: true,
+//                   itemBuilder: (context, index) {
+//                     final result = data?[index];
+//                     return ListTile(
+//                       title: Text(result?["userId"].toString() ?? ''),
+//                       subtitle: Text(result?["title"].toString() ?? ''),
+//                     );
+//                   },
+//                 ),
+//               );
+//             },
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
